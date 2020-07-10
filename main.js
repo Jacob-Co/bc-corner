71d8b74a7e87dad7c12f933c3f6aef0f7772ebea
Jacobtco <jacobtco@gmail.com>
2020-07-10 16:37:41 +0800

    Added revolving photos

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const underConstruction = document.querySelectorAll(".under-construction");
+const homePagePhoto = document.querySelector("#homepage-photo");
 
 console.log(underConstruction);
 console.log([...underConstruction]);
@@ -6,3 +7,13 @@ console.log([...underConstruction]);
 [...underConstruction].forEach((node) => node.addEventListener("click", function() {
   alert('Still Under Construction');
 }));
+
+let photoSources = ["sofitel.jpg", "colgate.jpg", "earlyBird.jpg"]
+let currentPhotoPosition = 0
+
+setInterval(function() {
+  currentPhotoPosition += 1;
+  currentPhotoPosition %= photoSources.length;
+  homePagePhoto.src = photoSources[currentPhotoPosition]
+  console.log('changed');
+}, 3000);
